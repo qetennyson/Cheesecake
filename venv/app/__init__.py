9e5f092965989b927d7fe50f6a3e3b08889b3092
qetennyson <quincytennyson8@gmail.com>
2018-11-06 16:30:49 -0500

    adding profiles / avatars

--- venv/app/__init__.py
+++ venv/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # The actual instance of Flask and a member of the app package.
 app = Flask(__name__)
@@ -12,6 +13,10 @@ app.config.from_object(Config)
 # This represents our actual db object.
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Login object
+login = LoginManager(app)
+login.login_view = 'login'
 
 # This is referencing the package and __init__.py script seen here.
 # The import is located here to avoid "circular imports" common to Flask.
--- venv/qblog.py
+++ venv/qblog.py
@@ -1,3 +1,15 @@
 from app import app
+
 # This top level script defines the Flask application instance.
 # We are importing the app variable, containing the Flask instance, from the app package.
+
+# This import also preps our Flask shell so that we can easily experiment in the
+# Python interpreter without repeatedly invoking the imports.
+from app import db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+
