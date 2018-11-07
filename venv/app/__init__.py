a38c8384c9a158d18d998b6f3cfb1454a6c73fff
qetennyson <quincytennyson8@gmail.com>
2018-11-06 20:07:55 -0500

    adding Followers

--- venv/app/__init__.py
+++ venv/app/__init__.py
@@ -1,3 +1,5 @@
+import logging
+from logging.handlers import SMTPHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -21,5 +23,25 @@ login.login_view = 'login'
 # This is referencing the package and __init__.py script seen here.
 # The import is located here to avoid "circular imports" common to Flask.
 
+# Setting up error logging to email when in production
+
+"""Creates an SMTPHandler instance, sets level to report only errors, no warnings,
+info, or debug msgs, and attaches it to the app.logger object from Flask"""
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler =SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no_reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject="Cheesecake Blog Failure",
+                credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
 # Models defines the db structure
-from app import routes, models
+from app import routes, models, errors
--- venv/config.py
+++ venv/config.py
@@ -13,3 +13,10 @@ class Config:
 
     # Can signal app each time a change is about to be made to the DB.  Not needed here.
     SQLALCHEMY_TRACK_MODIFICTIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['quincytennyson8@gmail.com']
