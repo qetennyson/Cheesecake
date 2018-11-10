d2990fa44ee1df0d7c976b34515d4637654f71c1
qetennyson <quincytennyson8@gmail.com>
2018-11-10 14:41:42 -0500

    adding custom cli (bugged)

--- venv/app/forms.py
+++ venv/app/forms.py
@@ -1,48 +1,49 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
+from flask_babel import _, lazy_gettext as _1
 from app.models import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField(_1('Username'), validators=[DataRequired()])
+    password = PasswordField(_1('Password'), validators=[DataRequired()])
+    remember_me = BooleanField(_1('Remember Me'))
+    submit = SubmitField(_1('Sign In'))
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField(_1('Username'), validators=[DataRequired()])
+    email = StringField(_1('Email'), validators=[DataRequired(), Email()])
+    password = PasswordField(_1('Password'), validators=[DataRequired()])
     password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
+        _1('Repeat Password'), validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField(_1('Register'))
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError("Please use a different username.")
+            raise ValidationError(_1("Please use a different username."))
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError("Please use a different email address.")
+            raise ValidationError(_1("Please use a different email address."))
 
 class EditProfileForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
-    submit = SubmitField('Submit')
+    username = StringField(_1('Username'), validators=[DataRequired()])
+    about_me = TextAreaField(_1('About Me'), validators=[Length(min=0, max=140)])
+    submit = SubmitField(_1('Submit'))
 
 class PostForm(FlaskForm):
-    post = TextAreaField('Say something', validators=[
+    post = TextAreaField(_1('Say something'), validators=[
         DataRequired(), Length(min=1, max=140)])
-    submit = SubmitField('Submit')
+    submit = SubmitField(_1('Submit'))
 
 class ResetPasswordRequestForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = StringField('Request Password Reset')
+    email = StringField(_1('Email'), validators=[DataRequired(), Email()])
+    submit = StringField(_1('Request Password Reset'))
 
 class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField(_1('Password'), validators=[DataRequired()])
     password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Request Password Reset')
+        _1('Repeat Password'), validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField(_1('Request Password Reset'))
--- venv/qblog.py
+++ venv/qblog.py
@@ -1,11 +1,10 @@
-from app import app
-
+from app import app, db
 # This top level script defines the Flask application instance.
 # We are importing the app variable, containing the Flask instance, from the app package.
 
 # This import also preps our Flask shell so that we can easily experiment in the
 # Python interpreter without repeatedly invoking the imports.
-from app import db
+from app import cli
 from app.models import User, Post
 
 @app.shell_context_processor
